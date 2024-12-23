9ad44b1031584801f8726a27f40211294a507c35
axionbuster <axionbuster@gmail.com>
2024-12-22 17:34:51 -0800

    Apply improvement due to X user undefined behavior
    X/Twitter: @undebeha (undefined behavior)

--- src/Variadic/Composition.hs
+++ src/Variadic/Composition.hs
@@ -8,17 +8,17 @@ where
 infixr 9 .**.
 
 -- | Variadic composition
-class Compose a b c d e where
+class Compose a b c where
   -- | Compose two functions
-  (.**.) :: (a -> b) -> (c -> d) -> (c -> e)
+  (.**.) :: a -> b -> c
 
-instance Compose a b c a b where
+instance Compose (a -> b) (c -> a) (c -> b) where
   (.**.) = (.)
   {-# INLINE (.**.) #-}
 
 instance
-  (Compose a b f d e) =>
-  Compose a b c (f -> d) (f -> e)
+  (Compose (a -> b) (f -> d) (f -> e)) =>
+  Compose (a -> b) (f -> f -> d) (f -> f -> e)
   where
   f .**. g = (.**.) f . g
   {-# INLINE (.**.) #-}
